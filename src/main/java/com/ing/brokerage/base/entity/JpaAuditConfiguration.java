61d126a2f9a2da3a3289a7d2d193b4ef1e32c24f
Can Şahintaş <can.sahintas@dopingtech.net>
2025-11-11 17:02:56 +0300

    minor corrections

--- src/main/java/com/ing/brokerage/base/entity/JpaAuditConfiguration.java
+++ src/main/java/com/ing/brokerage/base/entity/JpaAuditConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "jpaAuditorProvider", dateTimeProviderRef = "jpaAuditingDateTimeProvider")
-public class DopiJpaAuditConfiguration {
+public class JpaAuditConfiguration {
 
 
     /**
--- src/main/java/com/ing/brokerage/config/OpenApiConfiguration.java
+++ src/main/java/com/ing/brokerage/config/OpenApiConfiguration.java
@@ -4,28 +4,17 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import java.time.OffsetTime;
 import java.time.format.DateTimeFormatter;
 import org.springdoc.core.utils.SpringDocUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfiguration {
-
-    @Value("${dopi.api.title:Doping Tech API Title}")
-    String apiTitle;
-
-    @Value("${dopi.api.description:Doping Tech API Description}")
-    String apiDescription;
-
-    @Value("${dopi.api.version:v1.0}")
-    String apiVersion;
 
     static {
         var schema = new Schema<OffsetTime>();
@@ -54,17 +43,12 @@ public class OpenApiConfiguration {
                     )
             )
             .info(new Info()
-                .title(apiTitle)
-                .description(apiDescription)
-                .version(apiVersion)
+                .title("Brokerage")
+                .description("ING Brokerage System")
+                .version("v1")
                 .contact(new Contact()
-                    .name("Doping Technology")
-                    .url("https://dopingtech.net")
-                    .email("info@dopingtech.com"))
-                .termsOfService("https://www.dopinghafiza.com/kurumsal/34/iptal-iade-sartlari.html")
-                .license(new License()
-                    .name("License")
-                    .url("https://www.dopinghafiza.com/kurumsal/100/kvkk-aydinlatma-metni.html"))
+                    .name("Can Şahintaş")
+                    .email("can.sahintas@gmail.com"))
             );
     }
 }
